f683e6caa7cdd3a7f00ff3f1b5fe5caddd34dfed
Arzikristia <83139306+Arzikristia@users.noreply.github.com>
2021-12-04 13:32:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m, puppeteer) {
+export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
     const app = express();
     const path = import.meta.url.substring(7);
     const headersHTML = {'Content-Type':'text/html; charset=utf-8',...CORS}
     const headersTEXT = {'Content-Type':'text/plain',...CORS}
     const headersJSON={'Content-Type':'application/json',...CORS}
+    const headersCORS={...CORS}; 
 
     app    
         .use(bodyParser.urlencoded({extended:true}))  
@@ -37,6 +38,22 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
             const {addr} = r.body;
             r.res.send(addr)
         })
+        .all('/wordpress/', r =>{
+            r.res.set(headersJSON).send({
+                id: 1,
+                title: {
+                  rendered: "itmo310267",
+                },
+              });
+        })
+        .all("/wordpress/wp-json/wp/v2/posts/", (r) => {
+          r.res.set(headersJSON).send([{
+            id: 1,
+            title: {
+              rendered: "itmo310267",
+            },
+          }]);
+        })
         .post('/insert/', async r=>{
             r.res.set(headersTEXT);
             const {login,password,URL}=r.body;
@@ -45,30 +62,29 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
                 await m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
                 try{
                     await newUser.save();
-                    r.res.status(201).json({'Добавлено: ':login});
+                    r.res.status(201).json({'Add: ':login});
                 }
                 catch(e){
-                    r.res.status(400).json({'Ошибка: ':'Нет пароля'});
+                    r.res.status(400).json({'Error: ':'No password'});
                 }
             }
             catch(e){
                 console.log(e.codeName);
             }      
         })
-        .all('/test/', async r=>{
-            r.res.set(headersTEXT)
-            const {URL} = r.query;
-            console.log(URL)
-            const browser = await puppeteer.launch({headless: true, args:['--no-sandbox','--disable-setuid-sandbox']});
-            const page = await browser.newPage();
-            await page.goto(URL);
-            await page.waitForSelector("#inp");
-            await page.click('#bt');
-            const got = await page.$eval('#inp',el=>el.value);
-            console.log(got);
-            browser.close()
-            r.res.send(got)
+        .all('/render/',async(req,res)=>{
+            res.set(headersCORS);
+            const {addr} = req.query;
+            const {random2, random3} = req.body;
             
+            http.get(addr,(r, b='') => {
+                r
+                .on('data',d=>b+=d)
+                .on('end',()=>{
+                    fs.writeFileSync('views/index.pug', b);
+                    res.render('index',{login:'itmo310267',random2,random3})
+                })
+            })
         })
         .use(({res:r})=>r.status(404).set(headersHTML).send('itmo310267'))
         .set('view engine','pug')
--- index.js
+++ index.js
@@ -5,12 +5,11 @@ import bodyParser from 'body-parser';
 import m from 'mongoose';
 import crypto from 'crypto';
 import http from 'http';
-import puppeteer from 'puppeteer'
 import CORS from './CORS.js';
 import UserModel from './models/User.js';
 const User = UserModel(m);
-const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m, puppeteer);
-const PORT = process.env.PORT || 443;
+const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m);
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 
 
